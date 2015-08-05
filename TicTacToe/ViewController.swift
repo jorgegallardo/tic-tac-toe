ee5cc2f4cf43983aae6a01d26816136f866f3119
Jorge Gallardo <jorge8486@gmail.com>
2015-08-05 16:23:46 -0400

    determine winning board combos and winner

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,6 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 1 = noughts, 2 = crosses
+    var activePlayer = 1
+    
+    var gameActive = true
+    
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    
+    var winningCombinations = [
+        //horizontal
+        [0,1,2], [3,4,5], [6,7,8],
+        //vertical
+        [0,3,6], [1,4,7], [2,5,8],
+        //diagonals
+        [0,4,8], [2,4,6]
+    ]
 
     @IBOutlet weak var button: UIButton!
     
@@ -23,8 +39,32 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonPressed(sender: AnyObject) {
-        let image = UIImage(named: "cross.png")
+        
+        if gameState[sender.tag] == 0 && gameActive == true {
+        
+        var image = UIImage()
+        gameState[sender.tag] = activePlayer
+        
+        if activePlayer == 1 {
+            image = UIImage(named: "nought.png")!
+            activePlayer = 2
+        } else {
+            image = UIImage(named: "cross.png")!
+            activePlayer = 1
+        }
         sender.setImage(image, forState: .Normal)
+                
+            for combination in winningCombinations {
+                //check if gameState shows a winning combination
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                    if gameState[combination[0]] == 1 {
+                        print("Noughts has won!")
+                    } else {
+                        print("Crosses has won!")
+                    }
+                    gameActive = false
+                }
+            }
+        }
     }
-
 }
